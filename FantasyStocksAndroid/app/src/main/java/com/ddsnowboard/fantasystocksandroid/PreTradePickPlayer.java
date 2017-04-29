659540f2e95a21851765af92862261cebc754a26
Will Koster <jameswk2@illinois.edu>
2017-04-29 15:34:20 -0500

    Try to fix the player picker
    It didn't work. Maybe later...

--- FantasyStocksAndroid/app/src/main/java/com/ddsnowboard/fantasystocksandroid/PreTradePickPlayer.java
+++ FantasyStocksAndroid/app/src/main/java/com/ddsnowboard/fantasystocksandroid/PreTradePickPlayer.java
@@ -3,9 +3,11 @@ package com.ddsnowboard.fantasystocksandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,16 +25,20 @@ import static com.ddsnowboard.fantasystocksandroid.Utilities.FLOOR_ID;
 import static com.ddsnowboard.fantasystocksandroid.Utilities.UNKNOWN_ID;
 
 public class PreTradePickPlayer extends AppCompatActivity {
+    public static final String TAG = "PreTradePickPlayer";
 
     public static PreTradePickPlayer currentPlayerPicker;
 
     EditText searchBox;
     RecyclerView list;
-    private ArrayList<Player> allPlayers;
-    private ArrayList<Player> currentlyShownPlayers;
+    private ArrayList<Player> allPlayers = new ArrayList<>();
+    private ArrayList<Player> currentlyShownPlayers = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ***********
+        // for some reason there are no players showing up in the player picker. I don't know
+        // why not. It might be because it doesn't show floors or something? I need more print statements.
         super.onCreate(savedInstanceState);
         currentPlayerPicker = this;
         setContentView(R.layout.activity_pick_player);
@@ -42,29 +48,43 @@ public class PreTradePickPlayer extends AppCompatActivity {
         Intent intent = getIntent();
         int floorId = intent.getIntExtra(FLOOR_ID, UNKNOWN_ID);
         FantasyStocksAPI api = FantasyStocksAPI.getInstance();
-        GetPlayersTask task = new GetPlayersTask(this, players -> Arrays.stream(players)
-                .filter(p -> !p.getUser().equals(api.getUser()))
-                .peek(p -> currentlyShownPlayers.add(p))
-                .forEach(p -> allPlayers.add(p)));
+        Adapter adapter = new Adapter(currentlyShownPlayers);
+        list.setAdapter(adapter);
+        list.setLayoutManager(new LinearLayoutManager(this));
+        Log.d(TAG, String.valueOf(floorId));
+        GetPlayersTask task = new GetPlayersTask(this,
+                players -> {
+                    Arrays.stream(players)
+                            .filter(p -> !p.getUser().equals(api.getUser()))
+                            .peek(p -> currentlyShownPlayers.add(p))
+                            .forEach(p -> allPlayers.add(p));
+                    adapter.notifyDataSetChanged();
+                });
 
         task.execute(() -> floorId);
 
-        Adapter adapter = new Adapter(currentlyShownPlayers);
-        list.setAdapter(adapter);
 
         searchBox.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
             @Override
             public void afterTextChanged(Editable editable) {
                 currentlyShownPlayers.clear();
-                String searchText = editable.toString();
-                allPlayers.stream()
-                        .filter(p -> p.getUser().getUsername().contains(searchText))
-                        .forEach(currentlyShownPlayers::add);
+                if (editable.length() == 0) {
+                    currentlyShownPlayers.addAll(allPlayers);
+                } else {
+                    String searchText = editable.toString();
+                    allPlayers.stream()
+                            .filter(p -> p.getUser().getUsername().contains(searchText))
+                            .forEach(currentlyShownPlayers::add);
 
+                }
                 adapter.notifyDataSetChanged();
             }
         });
@@ -76,6 +96,7 @@ public class PreTradePickPlayer extends AppCompatActivity {
         public Adapter(ArrayList<Player> players) {
             this.players = players;
         }
+
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_stock, parent, false);
@@ -105,6 +126,7 @@ public class PreTradePickPlayer extends AppCompatActivity {
         }
 
         public void bind(Player p) {
+            Log.d(TAG, "Bound " + p.getUser().getUsername());
             name.setText(p.getUser().getUsername());
             holder.setOnClickListener(view -> {
                 Intent intent = new Intent(PreTradePickPlayer.this, FirstLevelTrade.class);
